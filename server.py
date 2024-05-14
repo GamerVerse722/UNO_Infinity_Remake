e00cdf6aaf60d65eed18146e3869fa14fb396405
GamerVerse722 <cristian987654321rios@gmail.com>
2024-05-14 15:41:52 -0500

    began migration of server.py

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, session, redirect, request, url_for, jsonify
 from flask_socketio import SocketIO, join_room, leave_room  # type: ignore
-from utilities.function import Room, Usertime
+# from utilities.function import Room, Usertime  # type: ignore
+from unogame.logging.log_wrapper import LoggerWrapper
+from utilities.function import Usertime  # type: ignore
+from unogame.room.room import Room
 # import threading
 import time
 
@@ -8,7 +11,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
-rooms = Room()
+logger: LoggerWrapper = LoggerWrapper('logs/latest.log', console=True)
+room: Room = Room(log_wrapper=logger)
 user_time = Usertime(time_interval=0.1)
 
 
@@ -27,8 +31,13 @@ def main():
 @app.route('/create', methods=['GET', 'POST'])
 def form_room_create():
     if request.method == 'POST':
-        code: str = rooms.create_room(request.form['room_name'])
-        player_uuid: str = rooms.add_player(code, request.form['username'])
+        room_name = request.form.get('room_name', '')
+        user_name = request.form.get('username', '')
+        if room_name == '' or user_name == '':
+            return redirect(url_for('main'))
+
+        code: str = room.create_room(room_name=room_name, code_length=6, max_user=2)
+        player_uuid: str = room.rooms[code].add_user(username=user_name)
 
         session['UUID'] = player_uuid
         session['Room'] = code
@@ -40,35 +49,39 @@ def form_room_create():
 @app.route('/join', methods=['GET', 'POST'])
 def form_room_join():
     if request.method == 'POST':
-        code: str = request.form['room_code']
-        if rooms.room_exist(code):
-            player_uuid: str = rooms.add_player(code, request.form['username'])
-            session['UUID'] = player_uuid
-            session['Room'] = code
-            return redirect(f'/room/{code}')
-        else:
+        room_name: str = request.form.get('room_name', '')
+        user_name: str = request.form.get('username', '')
+        code: str = request.form.get('room_code', '')
+
+        if room_name == '' or user_name == '' or code == '' or room.room_exist(code=code) is False:
             return redirect(url_for('main'))
+
+
+        player_uuid: str = room.rooms[code].add_user(username=user_name)
+        session['UUID'] = player_uuid
+        session['Room'] = code
+        return redirect(f'/room/{code}')
 
     return redirect(url_for('main'))
 
 
 @app.route('/data')
 def data():
-    return jsonify(rooms.__dict__())
+    return jsonify(room.__dict__())
 
 @app.route('/writefile')
 def writefile():
-    rooms.write_file('saved/sample.json')
+    room.__write__('saved/sample.json')
     return '<h1>File Written</h1>'
 
 
 @app.route('/room/<code>')
-def room(code):
-    if session.get('UUID') is None:
+def room_code(code):
+    if session.get('UUID') is None or room.room_exist(code) is False:
         return redirect(url_for('main'))
 
-    if rooms.room_exist(code) and rooms.room_member_exist(code, session.get('UUID', 'Null')):
-        return render_template("room.html", code=code, title=rooms.get_room_name(code))
+    if room.rooms[code].user_exist(user_uuid=session.get('UUID', 'Null')):
+        return render_template("room.html", code=code, title=room.rooms[code].room_name)
 
     else:
         return redirect(url_for('main'))
@@ -76,17 +89,30 @@ def room(code):
 
 @socketio.on('room join')
 def room_join():
-    if rooms.room_exist(session.get('Room', False)):
-        join_room(session['Room'])
-        join_room(session['UUID'])
-        socketio.emit('member_list', rooms.get_room_members(session['Room']), to=session['Room'])
-        socketio.emit('message_history', rooms.get_message_history(session['Room']), to=session['UUID'])
+    session_room = session.get('Room', '')
+    session_uuid = session.get('UUID', '')
+    if session_room == '' or session_uuid == '' or room.room_exist(session_room) is False:
+        return
+
+
+    join_room(session_room)
+    join_room(session_uuid)
+    socketio.emit('member_list', room.rooms[session_room].members_list, to=session['Room'])
+    socketio.emit('message_history', room.rooms[session_room].message_history, to=session['UUID'])
 
 
 @socketio.on('room messages')
 def handle_message(message):
-    if rooms.room_exist(session.get('Room', False)):
-        socketio.emit('message_history', [rooms.add_message(session['Room'], session['UUID'], message)], to=session['Room'])
+    session_room = session.get('Room', '')
+    session_uuid = session.get('UUID', '')
+    if session_room == '' or session_uuid == '' or room.room_exist(session_room) is False:
+        return
+
+    message_dict: dict | None = room.rooms[session_room].add_message(session_uuid, message)
+    if message is None:
+        return
+
+    socketio.emit('message_history', [message_dict], to=session['Room'])
 
 @socketio.on('ping_server')
 def ping_server(message):
